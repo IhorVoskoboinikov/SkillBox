25f3aed83e6ebbdd03a90f8700465a9f901613ee
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-05-02 12:13:06 +0300

    Снегопад

--- HW/HW_4/05_snowfall.py
+++ HW/HW_4/05_snowfall.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-
 import simple_draw as sd
 
+sd.resolution = (1200, 800)
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
@@ -9,6 +8,14 @@ import simple_draw as sd
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
 N = 20
+snowflakes = [
+    [700, 100, 10],
+    [750, 150, 20],
+    [800, 200, 30],
+    [650, 50, 40],
+    [700, 300, 50]
+]
+
 
 # Пригодятся функции
 # sd.get_point()
@@ -18,11 +25,27 @@ N = 20
 # sd.user_want_exit()
 
 # TODO здесь ваш код
+# y = snowflakes[0][0]
+# x = snowflakes[0][1]
+#
+# y1 = snowflakes[1][0]
+# x1 = snowflakes[1][1]
+#
+# y2 = snowflakes[2][0]
+# x2 = snowflakes[2][1]
+
 while True:
     sd.clear_screen()
-    pass
-    pass
-    pass
+
+    for point in snowflakes:
+            next_point = sd.get_point(point[1], point[0])
+            sd.snowflake(center=next_point, length=point[2])
+            point[0] -= 10
+            point[1] += 10
+
+    if point[0] < 50:
+        break
+
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
@@ -43,5 +66,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-
