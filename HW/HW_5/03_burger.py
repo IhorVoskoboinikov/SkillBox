cd8cfb269ade1d961eb1860b97829cb530e4e186
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-05-03 17:26:54 +0300

    my_burger

--- HW/HW_5/03_burger.py
+++ HW/HW_5/03_burger.py
@@ -15,4 +15,6 @@
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
-# TODO здесь ваш код
+from my_burger import cooking_a_cheeseburger
+
+cooking_a_cheeseburger()
--- HW/HW_5/my_burger.py
+++ HW/HW_5/my_burger.py
@@ -2,32 +2,34 @@ def bun(stage):
     if stage == 1:
         print("Берем одну булочку и режим ее на две половины, берем одну половину")
     else:
-        print("Накрываем второй половиной булочки\nЧисбургер готов!!!")
+        print("Финальный штрих - накрываем второй половиной булочки\nЧисбургер готов!!!")
 
 
 def cutlet():
-    print("Кладем котлету сверху")
+    print("Добавляем котлету сверху")
+
 
 def cucumber():
     print("Добавляем нарезаный слайсами огурец (3-4 шт)")
 
+
 def tomato():
     print("Добавляем нарезаный слайсами помидор (1-2 шт)")
 
+
 def mayonnaise():
     print("Добавляем майонез")
+
 
 def cheese():
     print("Добавляем ломтик сыра")
 
 
 def cooking_a_cheeseburger():
-    bun(1)
+    bun(stage=1)
     mayonnaise()
     cucumber()
     tomato()
     cutlet()
     cheese()
-    bun(2)
-
-cooking_a_cheeseburger()
+    bun(stage=2)
