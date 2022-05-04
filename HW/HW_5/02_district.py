0937633a6c2e7a4f7e0a2f2094112e7e8d776909
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-05-04 12:45:10 +0300

    Люди живущие в районе - финал

--- HW/HW_5/02_district.py
+++ HW/HW_5/02_district.py
@@ -5,6 +5,7 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
+'''Вариант №1. Мой'''
 
 from district.central_street.house1 import room1 as dcs_h1_r1
 from district.central_street.house1 import room2 as dcs_h1_r2
@@ -18,40 +19,37 @@ from district.soviet_street.house1 import room2 as dss_h1_r2
 from district.soviet_street.house1 import room1 as dss_h2_r1
 from district.soviet_street.house1 import room2 as dss_h2_r2
 
-print(f"В районе живут:, {','.join(dcs_h1_r1.folks)},{','.join(dcs_h1_r2.folks)},{','.join(dcs_h2_r1.folks)},{','.join(dcs_h2_r2.folks)},"
-      f"{','.join(dss_h1_r1.folks)},{','.join(dss_h1_r2.folks)},{','.join(dss_h2_r1.folks)},{','.join(dss_h2_r2.folks)},")
-
-
-# people_in_the_area = []
-# people_in_the_area = [
-#                       dcs_h1_r1.folks +
-#                       dcs_h1_r2.folks +
-#                       dcs_h2_r1.folks +
-#                       dcs_h2_r2.folks +
-#                       dss_h1_r1.folks +
-#                       dss_h1_r2.folks +
-#                       dss_h2_r1.folks +
-#                       dss_h2_r2.folks]
-
-
-
-# people_in_the_area.append(dcs_h1_r1.folks)
-# people_in_the_area.append(dcs_h1_r2.folks)
+# people = dcs_h1_r1.folks +\
+#          dcs_h1_r2.folks +\
+#          dcs_h2_r1.folks +\
+#          dcs_h2_r2.folks +\
+#          dss_h1_r1.folks +\
+#          dss_h1_r2.folks +\
+#          dss_h2_r1.folks +\
+#          dss_h2_r2.folks
 #
-# people_in_the_area.append(dcs_h2_r1.folks)
-# people_in_the_area.append(dcs_h2_r2.folks)
-#
-# people_in_the_area.append(dss_h1_r1.folks)
-# people_in_the_area.append(dss_h1_r2.folks)
-#
-# people_in_the_area.append(dss_h2_r1.folks)
-# people_in_the_area.append(dss_h2_r2.folks)
-#
-#
-#
-#
-# print(f"В районе живут:{','.join(str(people_in_the_area))}")
+# print(f"В районе живут: {','.join(people)}")
+# ============================================================================
+"""Вариант №2. мой"""
+people = str(dcs_h1_r1.folks +
+             dcs_h1_r2.folks +
+             dcs_h2_r1.folks +
+             dcs_h2_r2.folks +
+             dss_h1_r1.folks +
+             dss_h1_r2.folks +
+             dss_h2_r1.folks +
+             dss_h2_r2.folks)
 
+sheet_of_signs = ["'", "[", "]"]
 
+for i in sheet_of_signs:
+    people = people.replace(i, "")
 
+print("В районе живет:", people)
 
+"""Вариант решения от Вани"""
+# result = ‘’
+# for i, string in enumerate(strings_list):
+#     result += string
+#     if i < len(strings_list) - 1:  # если не последняя итерация
+#         result += ‘,’
