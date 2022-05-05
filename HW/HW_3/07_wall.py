d3eb59a91d93febcfbf19099a4a16af80eb8de7b
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-05-05 12:08:02 +0300

    Отрисовка стены на весь экран

--- HW/HW_3/07_wall.py
+++ HW/HW_3/07_wall.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
-
+import simple_draw as sd
+sd.resolution = (600, 600)
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
 
-simple_draw.pause()
+brig_x, brig_y = 100, 50
+row = 0
+for y in range(0, sd.resolution[1], brig_y):
+    row += 1
+    for x in range(0, sd.resolution[0], brig_x):
+        x0 = x if row % 2 else x + brig_x // 2
+        left_bottom = sd.get_point(x0, y)
+        right_top = sd.get_point(x0+brig_x, y+brig_y)
+        sd.rectangle(left_bottom=left_bottom, right_top=right_top, width=1)
+
+
+sd.pause()
