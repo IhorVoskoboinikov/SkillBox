95d1002b0a0bbb675663c37ff626edfc62a3a65f
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-05-31 13:43:13 +0300

    Статистика по буквам (первый вариант, все работает)

--- HW/HW_9/01_char_stat.py
+++ HW/HW_9/01_char_stat.py
@@ -21,7 +21,55 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+class FindNumberOfLetters:
+    alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
+                'Ф',
+                'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
+                'й',
+                'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю',
+                'я']
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.statistic = {}
+        self.total_quantity = 0
+        self.statistic_sorted = []
+
+    def open_file_statistic(self):
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for elem in line:
+                    if elem in self.alphabet:
+                        if elem in self.statistic:
+                            self.statistic[elem] += 1
+                        else:
+                            self.statistic[elem] = 1
+            self.statistic_sorted = sorted(self.statistic.items())
+
+    def create_table_to_print(self):
+        q = '+'
+        w = 'буква'
+        ch = 'частота'
+        t = 'итого'
+        table_header = (f'+{q:-^21}+\n'
+                        f'|{w: ^10}|{ch: ^10}|\n'
+                        f'+{q:-^21}+')
+
+        print(table_header)
+
+        for letter, quantity in self.statistic_sorted:
+            self.total_quantity += quantity
+            print(f'|{letter: ^10}|{quantity: ^10}|')
+
+        table_totals = (f'+{q:-^21}+\n'
+                        f'|{t: ^10}|{self.total_quantity: ^10}|\n'
+                        f'+{q:-^21}+')
+        print(table_totals)
+
+
+my_statistic = FindNumberOfLetters(file_name='voyna-i-mir.txt')
+my_statistic.open_file_statistic()
+my_statistic.create_table_to_print()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
