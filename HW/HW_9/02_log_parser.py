30c0884035fc74fd110e7a49821f31c2b599ec12
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-06-01 12:38:17 +0300

    ДЗ программа которая считывает файл. Первая версия.

--- HW/HW_9/02_log_parser.py
+++ HW/HW_9/02_log_parser.py
@@ -19,8 +19,37 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+class ReadFileParser:
 
+    def __init__(self, file_for_analysis, result_file):
+        self.file_name = file_for_analysis
+        self.result_file = result_file
+        self.file_to_write = {}
+        self.sort_file_to_write = []
+
+    def read_file(self):
+        with open(self.file_name, 'r', encoding='utf-8') as file:
+            self.sort_file(file)
+
+    def sort_file(self, file):
+        for line in file:
+            if 'NOK' in line:
+                if line[1:17] in self.file_to_write:
+                    self.file_to_write[line[1:17]] += 1
+                else:
+                    self.file_to_write[line[1:17]] = 1
+        self.sort_file_to_write = sorted(self.file_to_write.items())
+
+    def write_file(self):
+        with open(self.result_file, 'w', encoding='utf-8') as file:
+            for i, y in self.sort_file_to_write:
+                file.write('[' + i + ']' + '  -  ' + str(y) + '\n')
+
+
+parser = ReadFileParser(file_for_analysis='events.txt', result_file='result.txt')
+parser.read_file()
+parser.write_file()
+print(parser.sort_file_to_write)
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
