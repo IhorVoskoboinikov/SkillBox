cb5859cc5abefe468de0058bb0ea61b3efd48587
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-06-08 17:39:48 +0300

    ДЗ проверка регистрации (Финал с правками Вани)

--- HW/HW_10/03_registration_log.py
+++ HW/HW_10/03_registration_log.py
@@ -37,46 +37,50 @@ class FileVerification:
         self.registrations_good = registrations_good
         self.registrations_bad = registrations_bad
         self.list_registrations_good = []
-        self.list_registrations_bad = {}
+        self.list_registrations_bad = []
 
     def open_file(self):
         with open(self.file, 'r', encoding='utf-8') as file_to_check:
             for line in file_to_check:
-                line = line[:-1]
+                if line.endswith('\n'):
+                    line = line[:-1]
                 try:
                     self.check_all_fields(line)
                 except ValueError as ecx:
-                    self.list_registrations_bad[line] = ecx
+                    self.list_registrations_bad.append(f'[{line}] - {ecx}')
                     continue
                 except NotNameError:
-                    self.list_registrations_bad[line] = 'Поле имени содержит НЕ только буквы'
+                    self.list_registrations_bad.append(f'[{line}] - Поле имени содержит НЕ только буквы')
                     continue
                 except NotEmailError:
-                    self.list_registrations_bad[line] = 'Поле емейл НЕ содержит @ и .(точку)'
+                    self.list_registrations_bad.append(f'[{line}] - Поле емейл НЕ содержит @ и .(точку)')
                     continue
-                self.list_registrations_good.append(line)
+                self.list_registrations_good.append(f'[{line}]')
 
     def check_all_fields(self, line):
-        fild_name, fild_email, fild_age = line.split(' ')
-        fild_name = str(fild_name)
-        fild_email = str(fild_email)
-        fild_age = int(fild_age)
-        for letter in fild_name:
-            if not 'а' <= fild_name.lower() <= 'я' or letter == 'ё':
+        words = line.split(' ')
+        if len(words) != 3:
+            raise ValueError('Не все поля заполнены')
+        field_name, field_email, field_age = words
+        field_name = str(field_name)
+        field_email = str(field_email)
+        field_age = int(field_age)
+        for letter in field_name:
+            if not (('а' <= field_name.lower() <= 'я') or (letter == 'ё')):
                 raise NotNameError
-        if not 10 < fild_age < 99:
+        if not (10 < field_age < 99):
             raise ValueError('Поле возраст НЕ является числом от 10 до 99')
-        if not fild_email.count('@' or '.'):
+        if ('@' not in field_email) or ('.' not in field_email):
             raise NotEmailError
 
     def write_file(self):
         with open(self.registrations_bad, 'w', encoding='utf-8') as file_bad:
-            for i, y in self.list_registrations_bad.items():
-                file_bad.write('[' + i + ']' + ' - ' + str(y) + '\n')
+            for i in self.list_registrations_bad:
+                file_bad.write(i + '\n')
 
         with open(self.registrations_good, 'w', encoding='utf-8') as file_good:
             for i in self.list_registrations_good:
-                file_good.write('[' + i + ']' + '\n')
+                file_good.write(i + '\n')
 
 
 check = FileVerification(file='registrations.txt', registrations_good='registrations_good.log.txt',
