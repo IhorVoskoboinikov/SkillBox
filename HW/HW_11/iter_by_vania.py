a4c733f263f93ec947e856c748c6d5e0bdb9b293
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-06-14 17:45:56 +0300

    Доп задание по Итераторам от Вани (Финал все работает)

--- HW/HW_11/iter_by_vania.py
+++ HW/HW_11/iter_by_vania.py
@@ -9,19 +9,26 @@ class RandIntGenerator:
         self.n = n
 
     def __iter__(self):
+        self.n_found = False
         return self
 
     def __next__(self):
-        while self.n:
-            random_number = randint(self.a, self.b)
-            if random_number == self.n:
-                self.n = False
-                return random_number
-            return random_number
-        print(f"Нашли!")
-        raise StopIteration()
+        if self.n_found:
+            print(f"Нашли {self.n }")
+            raise StopIteration()
+        generated = randint(self.a, self.b)
+        if generated == self.n:
+            self.n_found = True
+        return generated
 
 
-iterator = RandIntGenerator(a=0, b=200, n=5)
+# iterator = RandIntGenerator(a=0, b=10, n=5)
+# for number in iterator:
+#     print(number)
+
+iterator = RandIntGenerator(a=0, b=10, n=5)
+for number in iterator:
+    print(number)
+
 for number in iterator:
     print(number)
