a7059b1135c8cb5fe2b030ab6862564f4d20c675
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-06-15 18:04:08 +0300

    Создание итератора для парсинга (первая версия, нужно переделать __next__). Все работает.

--- HW/HW_11/03_log_parser.py
+++ HW/HW_11/03_log_parser.py
@@ -14,4 +14,38 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+class LogParser:
+
+    def __init__(self, file_for_analysis):
+        self.file_name = file_for_analysis
+
+    def __iter__(self):
+        self.file_to_write = {}
+        self.sort_file_to_write = []
+        self.read_file()
+        self.i = -1
+        return self
+
+    def __next__(self):
+        self.i += 1
+        if self.i < len(self.sort_file_to_write):
+            return self.sort_file_to_write[self.i]
+        raise StopIteration()
+
+    def read_file(self):
+        with open(self.file_name, 'r', encoding='utf-8') as file:
+            self.sort_file(file)
+
+    def sort_file(self, file):
+        for line in file:
+            if line.endswith('NOK\n'):
+                if line[1:17] in self.file_to_write:
+                    self.file_to_write[line[1:17]] += 1
+                else:
+                    self.file_to_write[line[1:17]] = 1
+        self.sort_file_to_write = sorted(self.file_to_write.items())
+
+
+grouped_events = LogParser(file_for_analysis='events.txt')
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
