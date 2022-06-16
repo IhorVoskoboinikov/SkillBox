acda533d77a9f53a1fc63bfda744d3cd120ed6d1
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-06-16 17:31:51 +0300

    Создание итератора для парсинга (итератор+генератор). Все работает. Финальная версия.

--- HW/HW_11/03_log_parser.py
+++ HW/HW_11/03_log_parser.py
@@ -57,50 +57,6 @@
 # for group_time, event_count in grouped_events:
 #     print(f'[{group_time}] {event_count}')
 
-# class LogParser:
-#
-#     def __init__(self, file_for_analysis):
-#         self.file_name = file_for_analysis
-#
-#     def __iter__(self):
-#         self.file_to_write = {}
-#         self.sort_file_to_write = []
-#         self.read_file()
-#         self.i = 0
-#         # self.start = 0
-#         return self
-#
-#     def __next__(self):
-#         # if self.i == self.start:
-#         #     self.i += 1
-#         #     return self.sort_file_to_write[self.start]
-#         self.i += 1
-#         if self.i < len(self.sort_file_to_write):
-#             start = self.i
-#             if start == 1:
-#                 return self.sort_file_to_write[start-1]
-#             return self.sort_file_to_write[self.i]
-#         raise StopIteration()
-#
-#     def read_file(self):
-#         with open(self.file_name, 'r', encoding='utf-8') as file:
-#             self.sort_file(file)
-#
-#     def sort_file(self, file):
-#         for line in file:
-#             if line.endswith('NOK\n'):
-#                 if line[1:17] in self.file_to_write:
-#                     self.file_to_write[line[1:17]] += 1
-#                 else:
-#                     self.file_to_write[line[1:17]] = 1
-#         self.sort_file_to_write = sorted(self.file_to_write.items())
-#
-#
-# grouped_events = LogParser(file_for_analysis='events.txt')
-# for group_time, event_count in grouped_events:
-#     print(f'[{group_time}] {event_count}')
-
-
 
 # Через генератор
 
