d40ca416484d900858d06bcb818baaf8e1b663be
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-07-11 18:32:14 +0300

    ДЗ игра в боулинг на классах. Все работает.

--- HW/HW_14/bouling_engine.py
+++ HW/HW_14/bouling_engine.py
@@ -1,33 +1,41 @@
 from random import randint
 
-SKITTLES = 10
-FRAMES = 10
-_game_result_players = []
+
+class Bowling:
+
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.SKITTLES = 10
+        self.FRAMES = 10
+        self.game_result_player = []
+        self.final_result = None
+
+    def __str__(self):
+        return f'{self.player_name} закончил игру с результатом {self.final_result}'
+
+    def play_game(self):
+        while self.FRAMES:
+            first_throw = randint(0, self.SKITTLES)
+            second_throw = randint(0, (self.SKITTLES - first_throw))
+            for throw_number in range(1, 3):
+                if throw_number == 1:
+                    if first_throw == 0:
+                        self.game_result_player.append('-/')
+                        second_throw = randint(0, self.SKITTLES)
+                    elif 1 <= first_throw <= 9:
+                        self.game_result_player.append(str(first_throw))
+                    else:
+                        self.game_result_player.append('X')
+                        break
+                if throw_number == 2:
+                    if second_throw == 0:
+                        self.game_result_player.append('-')
+                        break
+                    self.game_result_player.append(str(second_throw))
+            self.FRAMES -= 1
+        self.final_result = ''.join(self.game_result_player)
 
 
-def play_game():
-    global FRAMES
-    first_throw = randint(0, SKITTLES)
-    second_throw = randint(0, (SKITTLES - first_throw))
-    for throw_number in range(1, 3):
-        if throw_number == 1:
-            if first_throw == 0:
-                _game_result_players.append('-/')
-                second_throw = randint(0, SKITTLES)
-            elif 1 <= first_throw <= 9:
-                _game_result_players.append(str(first_throw))
-            else:
-                _game_result_players.append('X')
-                break
-        if throw_number == 2:
-            if second_throw == 0:
-                _game_result_players.append('-')
-                break
-            _game_result_players.append(str(second_throw))
-    FRAMES -= 1
-
-
-while FRAMES:
-    res = play_game
-    res()
-print(_game_result_players)
+ihor = Bowling(player_name='Ihor')
+ihor.play_game()
+print(ihor.final_result)
