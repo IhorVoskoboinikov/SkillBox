e29e6c232cb7495fd50e3229c600a1aba30d773e
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-07-12 14:10:38 +0300

    ДЗ игра в боулинг доработаный вариант. С доработками от Вани.

--- HW/HW_14/bouling_engine.py
+++ HW/HW_14/bouling_engine.py
@@ -14,27 +14,23 @@ class Bowling:
         return f'{self.player_name} закончил игру с результатом {self.final_result}'
 
     def play_game(self):
-        while self.FRAMES:
+        for frame_i in range(self.FRAMES):
             first_throw = randint(0, self.SKITTLES)
             second_throw = randint(0, (self.SKITTLES - first_throw))
-            for throw_number in range(1, 3):
-                if throw_number == 1:
-                    if first_throw == 0:
-                        self.game_result_player.append('-')
-                    elif 1 <= first_throw <= 9:
-                        self.game_result_player.append(str(first_throw))
-                    else:
+            for throw_index, throw in enumerate([first_throw, second_throw]):
+                if throw == 0:
+                    self.game_result_player.append('-')
+                if throw == 10:
+                    if throw_index == 0:
                         self.game_result_player.append('X')
                         break
-                if throw_number == 2:
-                    if second_throw == 0:
-                        self.game_result_player.append('-')
-                        break
-                    if first_throw + second_throw == 10:
+                    else:
                         self.game_result_player.append('/')
-                        break
-                    self.game_result_player.append(str(second_throw))
-            self.FRAMES -= 1
+                if 1 <= throw <= 9:
+                    if throw_index == 1 and first_throw + second_throw == 10:
+                        self.game_result_player.append('/')
+                    else:
+                        self.game_result_player.append(str(throw))
         self.final_result = ''.join(self.game_result_player)
 
 
