6d7e3ff9e2cfe532f9b5d0e56f65e48d24d78a2a
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-07-13 11:40:20 +0300

    ДЗ игра в боулинг - функция просчета результата. Доп задание. Проверка на исключения.

--- HW/HW_14/bowling_additional.py
+++ HW/HW_14/bowling_additional.py
@@ -2,29 +2,32 @@ from bouling_engine import Bowling
 
 
 def get_score(game_result):
-    i = 0
-    n = len(game_result)
-    result = 0
-    multiplier = 1
-
-    while i < n:
-        curr_char = game_result[i]
-        if curr_char == 'X':
-            result += multiplier * 20
-            multiplier = 2
-            i += 1
-            continue
-
-        next_char = game_result[i + 1]
-
-        if next_char == '/':
-            result += multiplier * 15
-        else:
-            result += 0 if curr_char == '-' else (multiplier * int(curr_char))
-            result += 0 if next_char == '-' else (multiplier * int(next_char))
+    try:
+        i = 0
+        n = len(game_result)
+        result = 0
         multiplier = 1
-        i += 2
-    return result
+
+        while i < n:
+            curr_char = game_result[i]
+            if curr_char == 'X':
+                result += multiplier * 20
+                multiplier = 2
+                i += 1
+                continue
+
+            next_char = game_result[i + 1]
+
+            if next_char == '/':
+                result += multiplier * 15
+            else:
+                result += 0 if curr_char == '-' else (multiplier * int(curr_char))
+                result += 0 if next_char == '-' else (multiplier * int(next_char))
+            multiplier = 1
+            i += 2
+        return result
+    except Exception as exc:
+        return exc
 
 
 ihor = Bowling(player_name='Ihor')
