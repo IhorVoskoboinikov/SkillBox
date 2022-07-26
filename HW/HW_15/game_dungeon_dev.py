32d22a1e2bc99f89586999c4773aa2093b40cf3d
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-07-26 20:29:38 +0300

    ДЗ: Игра в лабиринт финал. Вариант №1. Все работает.

--- HW/HW_15/game_dungeon_dev.py
+++ HW/HW_15/game_dungeon_dev.py
@@ -1,8 +1,7 @@
 import datetime
 import json
-from pprint import pprint
-
-from termcolor import cprint
+import csv
+from termcolor import cprint, colored
 
 with open('rpg.json', 'r') as file_with_data:
     game_map = json.load(file_with_data)  # преобразование в словарь
@@ -11,6 +10,8 @@ with open('rpg.json', 'r') as file_with_data:
 
 
 class Game:
+    log_data = []
+
     def __init__(self, location_path):
         self.location_path = location_path
         self.experience = 0
@@ -18,14 +19,12 @@ class Game:
         self.start_game_time = datetime.datetime.now()
         self.locations = list(self.location_path.keys())
         self.current_location = None
+        self.current_location_name = None
         self.next_location = []
         self.monsters_in_location = []
-
-    # print(f"Data - {start_game_time.strftime('%d.%m.%Y!')}, time - {start_game_time.strftime('%H:%M:%S')}")
+        self.time_in_the_game = 0
 
     def game_step(self):
-        print(self.locations)
-        print(type(self.location_path))
         current_location = [self.location_path]
         for location_name in self.locations:
             for location_object in current_location:
@@ -37,7 +36,8 @@ class Game:
             else:
                 raise RuntimeError("Location path is wrong!")
 
-        self.current_location = str(self.locations[-1])
+        self.current_location = current_location
+        self.current_location_name = self.locations[-1]
 
         for elem in current_location:
             if isinstance(elem, dict):
@@ -48,47 +48,79 @@ class Game:
     def player_choose(self):
         n_monster = len(self.monsters_in_location)
         n_location = len(self.next_location)
-        print(f"Вы находитесь в {''.join(self.current_location)}\n"
-              f"У вас {self.experience} опыта и {self.remaining_time} секунд \n"
-              f"Прошло уже {self.remaining_time}\n"
-              f"Внутри вы видите:")
+        cprint(f"Вы находитесь в {''.join(self.current_location_name)}\n"
+               f"У вас {self.experience} опыта и {self.remaining_time} секунд \n"
+               f"Уже прошло времени - {self.time_in_the_game}", color='green')
+        cprint(f"Внутри вы видите:", color='blue')
         for num, monster in enumerate(self.monsters_in_location):
             print(f"{num + 1} -- Монстра {monster}")
         for num, loc in enumerate(self.next_location):
             print(f"{n_monster + (num + 1)} -- Вход в локацию {loc}")
-        print(f"Выберите действие:")
+        cprint(f"Выберите действие:", color='blue')
         for num, monster in enumerate(self.monsters_in_location):
             print(f"{num + 1} -- Атаковать монстра {monster}")
         for num, loc in enumerate(self.next_location):
             print(f"{n_monster + (num + 1)} -- Перейти в локацию {loc}")
-        player_choose = input(f'{n_monster + n_location + 1} -- Выход\n:')
-        if int(player_choose) == (n_monster + n_location + 1):
-            print("Вы покинули игру!")
-            self.remaining_time = 0
-        elif int(player_choose) <= n_monster:
-            self.monster_attack(player_choose)
-        else:
-            self.move_to_another_location(player_choose, n_monster)
+        cprint(f'{n_monster + n_location + 1} -- Выход')
+        player_choose = input(colored('Сделайте ваш выбор:', color='blue'))
+        try:
+            if int(player_choose) == (n_monster + n_location + 1) or self.experience >= 280:
+                cprint(f"Вы завершили игру с результатом:\n"
+                       f" - опыт = {self.experience}\n"
+                       f" - остаток времени = {self.remaining_time}", color='green')
+                self.remaining_time = 0
+            elif int(player_choose) <= n_monster:
+                self.monster_attack(player_choose)
+            else:
+                self.move_to_another_location(player_choose, n_monster)
+        except IndexError:
+            self.next_location.clear()
+            self.monsters_in_location.clear()
+            cprint("Не правильный ввод! Повторите попытку!", color='red')
 
     def monster_attack(self, player_choose):
         player_choose = int(player_choose)
         monster_name = self.monsters_in_location[(player_choose - 1)]
-        if len(self.locations) <= 1: #  место зависания!!!!
-            self.location_path[self.locations[0]].remove(monster_name)
-        else:
-            self.location_path[self.locations[0]][self.locations[1]].remove(monster_name)
+        exp = int(monster_name[(monster_name.find('exp') + 3):(monster_name.find('tm') - 1)])
+        spent_time_monster = float(monster_name[(monster_name.find('tm') + 2):])
+        self.experience += exp
+        self.remaining_time -= spent_time_monster
+        self.time_in_the_game += spent_time_monster
+        self.current_location.remove(monster_name)
+        self.monsters_in_location.remove(monster_name)
         self.next_location.clear()
         self.monsters_in_location.clear()
 
     def move_to_another_location(self, player_choose, n_monster):
+        self.start_game_time = datetime.datetime.now()
         player_choose = int(player_choose)
+        if not n_monster:
+            n_monster = 0
         location_name = self.next_location[(player_choose - n_monster - 1)]
+        spent_time = float(location_name[(location_name.find('tm') + 2):])
+        self.remaining_time -= spent_time
+        self.time_in_the_game += spent_time
         self.locations.append(location_name)
         self.next_location.clear()
+        self.monsters_in_location.clear()
+        self.start_game_time = datetime.datetime.now()
+        log_time = f"{self.start_game_time.strftime('%d.%m.%Y')} {self.start_game_time.strftime('%H:%M:%S')}"
+        Game.log_data.append([self.current_location_name, self.experience, log_time])
 
 
+field_names = ['current_location', 'current_experience', 'current_date']
 game = Game(location_path=game_map)
 while game.remaining_time > 0:
     cprint("================================", color='red')
     game.game_step()
     game.player_choose()
+# for i in game.log_data:
+#     print(i)
+#
+with open('dungeon.csv', 'w', newline='') as write_log_csv:
+    writer = csv.writer(write_log_csv)
+    writer.writerow(field_names)
+    for row in game.log_data:
+        writer.writerow(row)
+
+
