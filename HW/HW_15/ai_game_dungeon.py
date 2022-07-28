07125bceeded273ee214872a7d83a5c3ce0a2bde
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-07-28 11:06:56 +0300

    ДЗ: Игра ИИ в лабиринт, все работает последовательно.

--- HW/HW_15/ai_game_dungeon.py
+++ HW/HW_15/ai_game_dungeon.py
@@ -1,31 +1,37 @@
-import datetime
 import json
-import csv
 from termcolor import cprint, colored
 
 with open('rpg.json', 'r') as file_with_data:
     game_map = json.load(file_with_data)
 
-log_data = []
-next_location = []
-time_in_the_game = 0
+experience = 0
+remaining_time = 1234567890.0987654321
 
 
-def ai_game(game_map, experience=0, remaining_time=1234567890.0987654321):
-    current_location = ''.join(list(game_map.keys()))
-    print(current_location)
+def ai_game(game_map):
+    global remaining_time, experience
     for location, location_object in game_map.items():
-        print(location_object)
-        for object in location_object:
-            if not isinstance(object, dict):
-                exp = int(object[(object.find('exp') + 3):(object.find('tm') - 1)])
-                spent_time_monster = float(object[(object.find('tm') + 2):])
+        location = ''.join(location)
+        cprint(location, color='green')
+        for objects in location_object:
+            if not isinstance(objects, dict):
+                print(objects)
+                exp = int(objects[(objects.find('exp') + 3):(objects.find('tm') - 1)])
+                spent_time_monster = float(objects[(objects.find('tm') + 2):])
                 remaining_time -= spent_time_monster
                 experience += exp
-                del (game_map[current_location][0])
-                print("OK")
+                print(f'Exp - {experience}, time - {remaining_time}')
+                del objects
                 continue
-            ai_game(game_map=object)
+            location_name = ''.join(list(objects.keys()))
+            print(location_name)
+            spent_time = float(location_name[(location_name.find('tm') + 2):])
+            remaining_time -= spent_time
+            cprint(objects, color='red')
+            if (remaining_time > 0) and (experience < 280):
+                ai_game(game_map=objects)
+            else:
+                print(f"Game over! EXP = {experience}, TIME = {remaining_time}")
 
 
-ai = ai_game(game_map=game_map)
+ai_game(game_map=game_map)
