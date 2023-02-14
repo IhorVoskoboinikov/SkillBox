3396fe3176d64f7bd8454c2c325bb95d51eb4759
YOUR_NAME <YOUR_EMAIL>
2023-02-14 16:47:58 +0200

    Finally version of tests

--- Test_by_vanya/tests.py
+++ Test_by_vanya/tests.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
-
-
+import asyncio
 import unittest
 
-from test_tasks import test_parse_matrix, SOURCE_URL, EXPECTED
+from test_tasks import parse_matrix
+import config
 
 
 class MyParseMatrix(unittest.TestCase):
 
     def test_base(self):
-        result = test_parse_matrix(SOURCE_URL)
-        self.assertEqual(result, EXPECTED)
+        result = asyncio.run(parse_matrix(url=config.SOURCE_URL))
+        self.assertEqual(result, config.EXPECTED)
+
+    def test_wrong_type(self):
+        types = [int, float, complex, set, list, tuple, bool, None, dict]
+        for tp in types:
+            result = asyncio.run(parse_matrix(url=tp))
+            self.assertEqual(result, None)
+
+    def test_wrong_url(self):
+        wrong_url = "wrong.url.com"
+        result = asyncio.run(parse_matrix(url=wrong_url))
+        self.assertEqual(result, None)
 
 
 if __name__ == '__main__':
