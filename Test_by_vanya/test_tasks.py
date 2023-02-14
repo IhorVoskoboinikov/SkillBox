57b033b719b1e58a7317ff0be08d7b1310910788
YOUR_NAME <YOUR_EMAIL>
2023-02-14 16:47:13 +0200

    Finally version

--- Test_by_vanya/test_tasks.py
+++ Test_by_vanya/test_tasks.py
@@ -2,38 +2,19 @@
 import asyncio
 import aiohttp
 
-SOURCE_URL = 'https://raw.githubusercontent.com/koury/pymx/main/source.txt'
-EXPECTED = [
-    160, 150, 140, 130,
-    90, 50, 10, 20,
-    30, 40, 80, 120,
-    110, 100, 60, 70,
-]
-
-ERROR_DICT = {
-    206: "Partial Content",
-    301: "Moved Permanently",
-    302: "Moved Temporarily",
-    304: "Not Modified",
-    400: "Bad Request",
-    403: "Forbidden",
-    404: "Not Found",
-    408: "Request Timeout ",
-    429: "Too Many Requests ",
-    500: "Internal Server Error",
-    522: "Connection Timed Out",
-    524: "A Timeout Occurred",
-}
+import config
 
 
-async def parse_matrix(url):
+async def parse_matrix(url: str) -> (list[int], None):
     if not isinstance(url, str):
-        print(f"Wrong data type, not a string")
+        print(config.WRONG_DATA)
         return
     if not url.startswith('https://'):
-        print(f"Wrong url! Request must start with 'https://'")
+        print(config.WRONG_URL)
         return
+
     matrix = []
+
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             if resp.status == 200:
@@ -42,12 +23,16 @@ async def parse_matrix(url):
                     if i.startswith("|"):
                         matrix.append(list(map(int, i.split("|")[1:-1])))
             else:
-                if resp.status in ERROR_DICT:
-                    raise Warning(f"Status code - {resp.status}: Error - {ERROR_DICT[resp.status]}")
+                if resp.status in config.ERROR_DICT:
+                    raise Warning(f"Status code - {resp.status}: Error - {config.ERROR_DICT[resp.status]}")
                 else:
                     raise Warning(f"Status code = {resp.status}, error - {resp}")
+    if not matrix:
+        print(config.NO_DATA_IN_MATRIX)
+        return
 
     revers_matrix = []
+
     for i in matrix[::-1]:
         revers_matrix.append(i[::-1])
 
@@ -57,15 +42,15 @@ async def parse_matrix(url):
     index_1, index_2, index_3, index_4 = 0, len(revers_matrix[0]) - 1, len(revers_matrix) - 1, 0
 
     while len(result) < check_number:
-        # линия 1
+
         for i in revers_matrix[index_1][index_4:index_2 + 1]:
             result.append(i)
         index_1 += 1
-        # линия 2
+
         for i in revers_matrix[index_1:index_3 + 1]:
             result.append(i[index_2])
         index_2 -= 1
-        # линия 3
+
         if index_4 == 0:
             for i in revers_matrix[index_3][index_2::-1]:
                 result.append(i)
@@ -74,19 +59,14 @@ async def parse_matrix(url):
             for i in revers_matrix[index_3][index_2:index_4 - 1:-1]:
                 result.append(i)
             index_3 -= 1
-        # # линия 4
+
         for i in revers_matrix[index_3:index_1 - 1:-1]:
             result.append(i[index_4])
         index_4 += 1
-
+    if result == config.EXPECTED:
+        print("Результат верный!")
     return result
 
 
-def test_parse_matrix():
-    asyncio.run(parse_matrix(url="25"))
-
-    # else:
-    #     print(asyncio.run(parse_matrix(url=SOURCE_URL)))
-
-
-test_parse_matrix()
+if __name__ == "__main__":
+    asyncio.run(parse_matrix(url=config.SOURCE_URL))
